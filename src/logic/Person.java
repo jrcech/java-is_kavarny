1ba08defffa56550151d6a1cf7ae9c5633ac4a60
Vítězslav Slavík <vitezslavik@gmail.com>
2017-12-18 21:33:27 +0100

    Person - add constructor, javadoc

--- src/logic/Person.java
+++ src/logic/Person.java
@@ -1,5 +1,10 @@
 package logic;
 
+/** Class Person
+ * @author slav02
+ * @version ZS 2017
+ **/
+
 public class Person {
 
     private int id;
@@ -9,6 +14,20 @@ public class Person {
     private String email;
     private boolean isAdmin;
 
-    
+    /** Constructor
+    * @param id id
+    * @param username   prezdivka
+    * @param firstName  jmeno
+    * @param lastName   prijmeni
+    * @param email      email
+    * @param isAdmin    je admin**/
+    public Person(int id, String username, String firstName, String lastName, String email, boolean isAdmin) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.isAdmin = isAdmin;
+    }
 
 }
