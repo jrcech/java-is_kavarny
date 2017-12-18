7518e910b95a2078bb5d56d8a8b1c34fe61f113f
Vítězslav Slavík <vitezslavik@gmail.com>
2017-12-18 21:15:13 +0100

    Create opening page

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,29 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    
+    public void start(Stage primaryStage){
+        BorderPane borderPane = new BorderPane();
+
+        //Tlacitko - prihlaseni
+        Button login = new Button();
+        login.setText("Přihlášení");
+
+        //Tlacitko - registrace
+        Button registration = new Button();
+        registration.setText("Registrace");
+
+        ////Window - setup
+        FlowPane center = new FlowPane();
+        center.setAlignment(Pos.CENTER);
+        center.getChildren().addAll(login, registration);
+
+        borderPane.setCenter(center);
+        Scene scene = new Scene(borderPane, 450, 300);
+        primaryStage.setTitle("Aplikace káva");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 
     public static void main(String[] args) {
         launch(args);
