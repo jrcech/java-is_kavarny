0a6febd6bcd1452dda48431ced763e6859af1a08
Vítězslav Slavík <vitezslavik@gmail.com>
2017-12-18 21:23:10 +0100

    Add variables - Person

---
+++ src/logic/Person.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Person {
+
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private boolean isAdmin;
+
+    
+
+}
