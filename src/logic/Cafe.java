e7c4c52c12b160b9c554b6bf2462eeaec12013be
Vítězslav Slavík <vitezslavik@gmail.com>
2017-12-18 21:41:55 +0100

    Add variables - Cafe

---
+++ src/logic/Cafe.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Cafe {
+
+    private int id;
+    private String name;
+    private String description;
+    private double rating;
+    private String address;
+    private String region;
+    private String coffeeBrand;
+    private String event;
+    private String specialOffer;
+}
