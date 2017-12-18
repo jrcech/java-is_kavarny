513901fa6c5651ec711b756e7a048c7ae5779a36
Vítězslav Slavík <vitezslavik@gmail.com>
2017-12-18 21:11:23 +0100

    Create IntelliJ project

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
