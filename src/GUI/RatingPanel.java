7100b2bd987027ea28227af3d83bc37ba5626970
Vítězslav Slavík <vitezslavik@gmail.com>
2018-01-22 20:51:34 +0100

    Add RatingPanel layout

--- src/GUI/RatingPanel.java
+++ src/GUI/RatingPanel.java
@@ -2,8 +2,11 @@ package GUI;
 
 import interfaces.Idatabase;
 import interfaces.Observer;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import logic.Person;
 import logic.Rating;
 
 public class RatingPanel extends VBox implements Observer  {
@@ -22,6 +25,35 @@ public class RatingPanel extends VBox implements Observer  {
     @Override
     public void update(){
         this.getChildren().clear();
-        
+        for(Rating rating : database.getSearchDatabase().getRating()){
+            if(rating.getIdCafe() == id){
+                
+                //LABEL - Uzivatel
+                Label userLabel = new Label();
+                userLabel.setText("Uživatel:");
+                Label userDataLabel = new Label();
+                for (Person person : database.getSearchDatabase().getPerson()) {
+                    if (person.getId() == rating.getIdPerson()) {
+                        userDataLabel.setText(person.getUsername());
+                        break;
+                    }
+                }
+
+                //LABEL - Rating
+                Label ratingLabel = new Label();
+                ratingLabel.setText("Hodnocení:");
+                Label ratingDataLabel = new Label();
+                ratingDataLabel.setText("" + rating.getRating());
+
+                //LABEL + TEXTAREA - Comment
+                Label commentLabel = new Label();
+                commentLabel.setText("Komentář:");
+                TextArea commentTextArea = new TextArea();
+                commentTextArea.setText(rating.getComment());
+                commentTextArea.setEditable(false);
+
+
+            }
+        };
     }
 }
